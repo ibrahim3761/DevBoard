17f32f5c297f84c07b189d937d1e568902b6027b
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-03-02 02:09:26 +0600

    done

--- script/utilities.js
+++ script/utilities.js
@@ -1,12 +1,17 @@
 function CompTask (id,e,card_title){
         e.preventDefault();
         const conTaskAssNum = parseInt(document.getElementById("task-ass-num").innerText);
+
         const conComTaskNum = parseInt(document.getElementById("comp-task-num").innerText);
+
         const taskTitle = document.getElementById(card_title).innerText;
         const currTaskAssNum = conTaskAssNum - 1;
+
         document.getElementById("task-ass-num").innerText = currTaskAssNum;
         const currComTaskNum = conComTaskNum + 1;
+
         document.getElementById("comp-task-num").innerText = currComTaskNum;
+        
         const disBtn = document.getElementById(id);
 
         const container = document.getElementById("task-complete-container");
