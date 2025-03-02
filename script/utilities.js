2d17d5f6246ce1ec820fc27777261ae9d53e86ff
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-03-02 14:15:01 +0600

    completed

--- script/utilities.js
+++ script/utilities.js
@@ -1,40 +1,38 @@
-function CompTask (id,e,card_title){
-        e.preventDefault();
-        const conTaskAssNum = parseInt(document.getElementById("task-ass-num").innerText);
+function CompTask(button, e, card_title) {
+    e.preventDefault();
 
-        const conComTaskNum = parseInt(document.getElementById("comp-task-num").innerText);
+    const conTaskAssNum = parseInt(document.getElementById("task-ass-num").innerText);
+    const conComTaskNum = parseInt(document.getElementById("comp-task-num").innerText);
+    const taskTitle = document.getElementById(card_title).innerText;
 
-        const taskTitle = document.getElementById(card_title).innerText;
-        const currTaskAssNum = conTaskAssNum - 1;
+    const currTaskAssNum = conTaskAssNum - 1;
+    document.getElementById("task-ass-num").innerText = "0" + currTaskAssNum;
 
-        document.getElementById("task-ass-num").innerText = currTaskAssNum;
-        const currComTaskNum = conComTaskNum + 1;
+    const currComTaskNum = conComTaskNum + 1;
+    document.getElementById("comp-task-num").innerText = currComTaskNum;
 
-        document.getElementById("comp-task-num").innerText = currComTaskNum;
-        
-        const disBtn = document.getElementById(id);
-
-        const container = document.getElementById("task-complete-container");
-        const div = document.createElement("div");
-        div.innerHTML = `
+    const container = document.getElementById("task-complete-container");
+    const div = document.createElement("div");
+    div.innerHTML = `
         <div class="p-3">
             <p class="bg-[#F4f7FF] p-3 rounded-lg">
               You have completed the task ${taskTitle} at ${getCurrentTime()}.
             </p>
           </div>
-        `
-        container.appendChild(div);
+        `;
 
+    container.appendChild(div);
 
-        disBtn.style.backgroundColor = "gray";
-        alert("Board Updated Successfully");
-        if (currTaskAssNum === 0) {
-            alert("Congrats!!! You have completed all the current task")
-        }
+    button.style.backgroundColor = "gray";
 
-        disBtn.classList.remove("cursor-pointer")
+    alert("Board Updated Successfully");
 
-        disBtn.disabled = true;
+    if (currTaskAssNum === 0) {
+        alert("Congrats!!! You have completed all the current tasks!");
+    }
+
+    button.classList.remove("cursor-pointer");
+    button.disabled = true;
 }
 
 
